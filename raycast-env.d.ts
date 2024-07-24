3f21bee77e5b54eb3a0dde444b7c99c3389b7707
Aziz Raissi Darouez <raissi.aziz@gmail.com>
2024-07-24 13:14:36 +0200

    1.0 Release Candidate
    Initial commit.
    
    ##Added :
    - Jokers
    - Planets
    - Tarot Cards
    - Boosters
    - Decks
    - Seals
    - Enhancements
    - Blinds
    - Spectral Cards
    - Vouchers / Upgraded Vouchers

---
+++ raycast-env.d.ts
@@ -0,0 +1,69 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `jokers` command */
+  export type Jokers = ExtensionPreferences & {}
+  /** Preferences accessible in the `tarot` command */
+  export type Tarot = ExtensionPreferences & {}
+  /** Preferences accessible in the `decks` command */
+  export type Decks = ExtensionPreferences & {}
+  /** Preferences accessible in the `planets` command */
+  export type Planets = ExtensionPreferences & {}
+  /** Preferences accessible in the `spectral` command */
+  export type Spectral = ExtensionPreferences & {}
+  /** Preferences accessible in the `voucher` command */
+  export type Voucher = ExtensionPreferences & {}
+  /** Preferences accessible in the `vouchersplus` command */
+  export type Vouchersplus = ExtensionPreferences & {}
+  /** Preferences accessible in the `enhancements` command */
+  export type Enhancements = ExtensionPreferences & {}
+  /** Preferences accessible in the `seals` command */
+  export type Seals = ExtensionPreferences & {}
+  /** Preferences accessible in the `tags` command */
+  export type Tags = ExtensionPreferences & {}
+  /** Preferences accessible in the `blinds` command */
+  export type Blinds = ExtensionPreferences & {}
+  /** Preferences accessible in the `boosters` command */
+  export type Boosters = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `jokers` command */
+  export type Jokers = {}
+  /** Arguments passed to the `tarot` command */
+  export type Tarot = {}
+  /** Arguments passed to the `decks` command */
+  export type Decks = {}
+  /** Arguments passed to the `planets` command */
+  export type Planets = {}
+  /** Arguments passed to the `spectral` command */
+  export type Spectral = {}
+  /** Arguments passed to the `voucher` command */
+  export type Voucher = {}
+  /** Arguments passed to the `vouchersplus` command */
+  export type Vouchersplus = {}
+  /** Arguments passed to the `enhancements` command */
+  export type Enhancements = {}
+  /** Arguments passed to the `seals` command */
+  export type Seals = {}
+  /** Arguments passed to the `tags` command */
+  export type Tags = {}
+  /** Arguments passed to the `blinds` command */
+  export type Blinds = {}
+  /** Arguments passed to the `boosters` command */
+  export type Boosters = {}
+}
+
+
